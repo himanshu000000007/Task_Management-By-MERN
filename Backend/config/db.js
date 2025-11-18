3411f804a28826956ef4447db2329892791a7538
Himanshu00000007 <Himanshubhukar545@gmail.com>
2025-11-18 16:07:51 +0530

    .env

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://akshaysinghrajput545_db_user:taskmanagement@cluster0.pkigetm.mongodb.net/taskify')
+    await mongoose.connect(process.env.MONGO_DB)
     .then(()=>console.log('DB connected'));
 }
